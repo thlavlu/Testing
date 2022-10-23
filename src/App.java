239f21a82a1ffdde5300b2be888d35ccd2db68ff
compboy <compboy01@gmail.com>
2022-10-24 00:01:35 +0600

    adding exception hangleling

--- src/App.java
+++ src/App.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        try{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter input");
         int input = sc.nextInt();
-        System.out.println("Hello, World! "+input);
+        System.out.println("The Output is "+input);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
+
 }
