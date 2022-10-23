0472f22b8693968de7553e7f8434acd322c8238c
compboy <compboy01@gmail.com>
2022-10-23 23:33:33 +0600

    init

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println("Hello, World! "+input);
+    }
+}
