76d375d39b45c96a90ad980e2abf71f76f8d5d9d
compboy <compboy01@gmail.com>
2022-10-23 23:43:21 +0600

    2nd testing

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input");
         int input = sc.nextInt();
         System.out.println("Hello, World! "+input);
     }
